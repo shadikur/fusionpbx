4372b4e09a830565aa71a1d8dd854495e7cdf17c
shadikur <me@shadikur.com>
2020-05-04 00:35:12 +0200

    FS 1.6 Repo Removed

--- fusionpbx-upgrade.sh
+++ fusionpbx-upgrade.sh
@@ -58,7 +58,7 @@ echo "Upgrading Switch ... \n"
 #!/bin/sh
 apt-get update && apt-get install -y curl memcached haveged
 curl https://files.freeswitch.org/repo/deb/debian/freeswitch_archive_g0.pub | apt-key add -
-echo "deb http://files.freeswitch.org/repo/deb/freeswitch-1.6/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
+#echo "deb http://files.freeswitch.org/repo/deb/freeswitch-1.6/ jessie main" > /etc/apt/sources.list.d/freeswitch.list
 echo "deb http://files.freeswitch.org/repo/deb/debian-unstable/ jessie main" >> /etc/apt/sources.list.d/freeswitch.list
 apt-get update
 apt-get install -y gnupg gnupg2
