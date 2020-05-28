f9e8eaaec2d7e62731da48a36e515ba11149645c
shadikur <me@shadikur.com>
2020-05-28 21:25:40 +0200

    Update

--- php-settings.php
+++ php-settings.php
@@ -4,13 +4,10 @@
 # Copyright (c) shadikur.com
 # Script follows here:
 
-#All the variables
-php_ini_file = $(/etc/php/7.3/fpm/php.ini)
-
 #Update PHP Settings
-sed 's#post_max_size = .*#post_max_size = 80M#g' -i ${php_ini_file}
-sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i ${php_ini_file}
-sed 's#;max_input_vars = .*#max_input_vars = 8000#g' -i ${php_ini_file}
-sed 's#; max_input_vars = .*#max_input_vars = 8000#g' -i ${php_ini_file}
+sed 's#post_max_size = .*#post_max_size = 80M#g' -i /etc/php/7.3/fpm/php.ini
+sed 's#upload_max_filesize = .*#upload_max_filesize = 80M#g' -i /etc/php/7.3/fpm/php.ini
+sed 's#;max_input_vars = .*#max_input_vars = 8000#g' -i /etc/php/7.3/fpm/php.ini
+sed 's#; max_input_vars = .*#max_input_vars = 8000#g' -i /etc/php/7.3/fpm/php.ini
 
 service nginx restart
