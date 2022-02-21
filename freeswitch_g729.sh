5a897b57d47b8e81e1751ceee2a96177bb1b3468
M S Rahman <shadikurbd@gmail.com>
2022-02-21 20:44:31 +0100

    Update freeswitch_g729.sh

--- freeswitch_g729.sh
+++ freeswitch_g729.sh
@@ -6,7 +6,7 @@
 
 #Download the codec 
 rm -rf /usr/lib/freeswitch/mod/mod_bcg729.so
-wget -P /usr/lib/freeswitch/mod http://develop.shadikur.com/fs/mod_bcg729.so
+wget -P /usr/lib/freeswitch/mod https://github.com/shadikur/fusionpbx/raw/master/mod_bcg729.so
 
 #Unload existing G729 codec
 /usr/bin/fs_cli -x "unload mod_g729"
